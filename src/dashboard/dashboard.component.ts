07f92370079868fbdc9dc69e0893e97af1751b6e
lbandin <lucasblandin@hotmail.fr>
2025-04-30 20:34:30 +0200

    Hero section and all the others

---
+++ src/dashboard/dashboard.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../app/hero';
+import { HeroService } from '../services/hero.service';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: [ './dashboard.component.css' ],
+  imports: [RouterLink, CommonModule]
+})
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+  }
+}
---
+++ src/hero-detail/hero-detail.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { Hero } from '../app/hero';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from '../services/hero.service';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-hero-detail',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './hero-detail.component.html',
+  styleUrl: './hero-detail.component.css'
+})
+export class HeroDetailComponent {
+  hero: Hero | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+}
