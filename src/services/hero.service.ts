d985147d3ef13232bb1d3ffd34e69020560db0b5
lbandin <lucasblandin@hotmail.fr>
2025-05-07 14:40:52 +0200

    Fix error merge

--- src/services/hero.service.ts
+++ src/services/hero.service.ts
@@ -1,13 +1,9 @@
-import { inject, Inject, Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Hero } from '../interface/hero';
-<<<<<<< Updated upstream
-import { HEROES } from '../app/mock-heroes';
-=======
 import { HEROES } from '../mock/mock-heroes';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment.development';
->>>>>>> Stashed changes
 
 @Injectable({
   providedIn: 'root',
@@ -17,15 +13,14 @@ export class HeroService {
   private http = inject(HttpClient);
   private apiUrl = environment.apiUrl + '/api/Heroes';
 
-  getHeroes(): Observable<any> {
-    return this.http.get(this.apiUrl);
-    //return of(HEROES);
+  getHeroes(): Observable<Hero> {
+    return this.http.get<Hero>(this.apiUrl);
   }
 
   getHero(id: number): Observable<Hero> {
     // For now, assume that a hero with the specified `id` always exists.
     // Error handling will be added in the next step of the tutorial.
-    const hero = HEROES.find(h => h.id === id)!;
+    const hero = HEROES.find((h: { id: number; }) => h.id === id)!;
     return of(hero);
   }
 }
